df7accf0a18a5190e657371128d290a1c7562d37
xloem <0xloem@gmail.com>
2022-02-02 21:59:42 +0000

    fixed dimension errors in tests. however, jax.jit appears to refuse to compile callback arguments, so a different approach may be appropriate

--- test/test_computation.py
+++ test/test_computation.py
@@ -1,9 +1,12 @@
 import unittest
-import torch
+import torch, jax
 import numpy as np
 from memory_efficient_attention import efficient_dot_product_attention_pt, efficient_dot_product_attention_jax
 from flax.linen.attention import dot_product_attention
 
+efficient_dot_product_attention_jax = jax.jit(efficient_dot_product_attention_jax)
+
+jax.config.update('jax_log_compiles', True)
 
 class ComputationTest(unittest.TestCase):
     @staticmethod
@@ -12,8 +15,8 @@ class ComputationTest(unittest.TestCase):
         Qb = np.random.rand(1, b, 128, 16, 8).astype("float32")
         Kb = np.random.rand(1, b, 128, 16, 8).astype("float32")
         Vb = np.random.rand(1, b, 128, 16, 8).astype("float32")
-        Mb = np.random.rand(1, b, 128, 16, 128) > 0.5
-        Bb = np.random.rand(1, b, 128, 16, 128).astype("float32") / 100
+        Mb = np.random.rand(1, b, 16, 128, 128) > 0.5
+        Bb = np.random.rand(1, b, 16, 128, 128).astype("float32") / 100
         return Qb, Kb, Vb, Mb, Bb
 
     @staticmethod
@@ -21,15 +24,18 @@ class ComputationTest(unittest.TestCase):
         # plain data
         Qb, Kb, Vb, Mb, Bb = ComputationTest.data()
 
-        def MBchunker(tensor):
-            def get_range(query_start, query_size, key_start, key_size):
-                return tensor[:,:,query_start:query_start+query_size,:,key_start:key_start+key_size]
-            return get_range
+        def MBchunker(tensor, requires_grad):
+            def get_range_jax(query_start, query_size, key_start, key_size):
+                return jax.lax.dynamic_slice(tensor, tuple([0] * (tensor.ndim - 2)) + (query_start, key_start),
+                        slice_sizes=tuple(tensor.shape[:-2]) + (query_size, key_size))
+            def get_range_torch(query_start, query_size, key_start, key_size):
+                return torch.tensor(tensor[:,:,:,query_start:query_start+query_size,key_start:key_start+key_size], requires_grad=requires_grad)
+            return dict(jax=get_range_jax, torch=get_range_torch)
 
         # mask broadcasting
-        for Mb_variant in (Mb, Mb[:,:,0,:,:], Mb[:,:,:,0,:], Mb[:,:,:,:,0], MBchunker(Mb)):
+        for Mb_variant in (Mb, Mb[:,:,:1,:,:], Mb[:,:,:,:1,:], Mb[:,:,:,:,:1], MBchunker(Mb, False)):
             # bias broadcasting
-            for Bb_variant in (Bb, Bb[:,:,0,:,:], Bb[:,:,:,0,:], Bb[:,:,:,:,0], MBchunker(Bb)):
+            for Bb_variant in (Bb, Bb[:,:,:1,:,:], Bb[:,:,:,:1,:], Bb[:,:,:,:,:1], MBchunker(Bb, True)):
                 yield Qb, Kb, Vb, Mb_variant, Bb_variant
 
     @staticmethod
@@ -38,18 +44,32 @@ class ComputationTest(unittest.TestCase):
         Qbt = torch.tensor(Qb, requires_grad=True)
         Kbt = torch.tensor(Kb, requires_grad=True)
         Vbt = torch.tensor(Vb, requires_grad=True)
-        Bbt = torch.tensor(Bb, requires_grad=True)
-        Mbt = torch.tensor(Mb)
+        if type(Bb) is dict:
+            Bbt = Bb['torch']
+        else:
+            Bbt = torch.tensor(Bb, requires_grad=True)
+        if type(Mb) is dict:
+            Mbt = Mb['torch']
+        else:
+            Mbt = torch.tensor(Mb)
         return efficient_dot_product_attention_pt(Qbt, Kbt, Vbt, Mbt, Bbt).detach().numpy()
 
     @staticmethod
     def calc_jax(data):
         Qb, Kb, Vb, Mb, Bb = data
+        if type(Mb) is dict:
+            Mb = Mb['jax']
+        if type(Bb) is dict:
+            Bb = Bb['jax']
         return np.asarray(efficient_dot_product_attention_jax(Qb, Kb, Vb, Mb, Bb))
 
     @staticmethod
     def calc_flax(data):
         Qb, Kb, Vb, Mb, Bb = data
+        if type(Mb) is dict:
+            Mb = Mb['jax'](0, Qb.shape[-3], 0, Kb.shape[-3])
+        if type(Bb) is dict:
+            Bb = Bb['jax'](0, Qb.shape[-3], 0, Kb.shape[-3])
         return np.asarray(dot_product_attention(Qb, Kb, Vb, Bb, Mb))
 
     def test_pt(self):
